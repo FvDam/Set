634f35674fac7e4b99225b32eeb54f3e996a1d29
FvDam <92367008+FvDam@users.noreply.github.com>
2022-08-01 22:13:39 +0200

    Added button array
    Also a script to make images

---
+++ EmptyImage.py
@@ -0,0 +1,37 @@
+import cv2
+import numpy as np
+
+# font
+font = cv2.FONT_HERSHEY_SIMPLEX
+  
+
+  
+# fontScale
+fontScale = 1
+   
+# Blue color in BGR
+color = (0, 255, 0)
+  
+# Line thickness of 2 px
+thickness = 2
+   
+image_height = 191
+image_width = 148
+number_of_color_channels = 3
+# color = (255,0,255)
+pixel_array = np.full((image_height, image_width, number_of_color_channels), color, dtype=np.uint8)
+
+cardsArray = [[a,b,c,d] for a in ['1','2','3'] for b in ['1','2','3'] for c in ['1','2','3'] for d in ['1','2','3']]
+for card in cardsArray:
+    imageText = ''.join(map(str, card))
+    cv2.imwrite(imageText+".png", pixel_array)
+
+# org
+org = (35, 110)
+color = (0, 0, 0)
+for card in cardsArray:
+    imageText = ''.join(map(str, card))
+    image = cv2.imread(imageText+".png")
+    image = cv2.putText(image, imageText, org, font, 
+                   fontScale, color, thickness, cv2.LINE_AA)
+    cv2.imwrite(imageText+".png", image)
--- Set.py
+++ Set.py
@@ -1,25 +1,34 @@
 from tkinter import *
 import random
 
-
+cardsCounter = 0
 counter = 0
 clickedArray = [[99, 99],[99, 99],[99, 99]]
-cardsArray = [[a, b, c, d] for a in [1,2,3] for b in [1,2,3] for c in [1,2,3] for d in [1,2,3]]
-random.shuffle(cardsArray)
+cardImageArray = [[0 for x in range(5)] for x in range(3)]
+cardsArray = [[a,b,c,d] for a in ['1','2','3'] for b in ['1','2','3'] for c in ['1','2','3'] for d in ['1','2','3']]
 
+# Function for filling initial button array
+def fillImageButtonArray():
+    global cardsCounter
+    global cardsArray
+    global cardImageArray
 
-print(cardsArray)
-def refreshButtonCollor(x, y):
+    for x in range(0, 3):
+        for y in range(0, 5):
+            cardImageArray[x][y] = PhotoImage(file=''.join(map(str, cardsArray[cardsCounter]))+".png")
+            cardsCounter = cardsCounter + 1
+
+def refreshButtonImage(x, y):
     global clickedArray
     global counter
     global clicked_btn
+    global cardsCounter
+    global cardImageArray
 
-    
-    # Reset colors
     for card_x, card_y in clickedArray:
-        btnCard[card_x][card_y].config(bg="SystemButtonFace", image=clicked_btn)
-        
-
+        cardImageArray[card_x][card_y] = PhotoImage(file=''.join(map(str, cardsArray[cardsCounter]))+".png")
+        cardsCounter = cardsCounter + 1
+        btnCardArray[card_x][card_y].config(bg="SystemButtonFace", image=cardImageArray[card_x][card_y])
 
     # Reset array and counter
     clickedArray = [[99, 99],[99, 99],[99, 99]]
@@ -35,26 +44,26 @@ def color_change(x, y):
             if clickedArray[i] == [x, y]:
                 counter = counter - 1
                 clickedArray[i] = [99, 99]
-                btnCard[x][y].config(bg="SystemButtonFace")
+                btnCardArray[x][y].config(bg="SystemButtonFace")
     else:
         # Search first empty spot in clicked array
         for i in range(0,3):
             if clickedArray[i] == [99, 99]:
                 clickedArray[i] = [x, y]
                 break
-        btnCard[x][y].config(bg="red")
+        btnCardArray[x][y].config(bg="red")
         counter = counter + 1
 
     print(clickedArray)
     
     if counter == 3:
-        refreshButtonCollor(x, y)
+        refreshButtonImage(x, y)
     print(x,y)
 
 window = Tk()
 
 window.title("Set")
-window.geometry("1500x800")
+window.geometry("1000x650")
 
 ## Score counter frame
 scoreFrame = Frame(window)
@@ -72,36 +81,39 @@ setExtraFrame = Frame(window)
 ## Button frame
 buttonFrame = Frame(window)
 
-btnCard = [[0 for x in range(5)] for x in range(3)]
-click_btn= PhotoImage(file='test.png')
-clicked_btn= PhotoImage(file='testing.png')
+btnCardArray = [[0 for x in range(5)] for x in range(3)]
+cardValueArray = [[0 for x in range(5)] for x in range(3)]
 
+
+emptyImage= PhotoImage(file='testEmpty.png')
+
+fillImageButtonArray()
 ## Row 1
 for y in range(0, 5):
     if y < 4:
-        btnCard[0][y] = Button(setUpperFrame, image=click_btn, command = lambda x=0, y=y: color_change(0, y))
-        btnCard[0][y].pack(side = LEFT, fill = BOTH, expand=True)
+        btnCardArray[0][y] = Button(setUpperFrame, image=cardImageArray[0][y], command = lambda x=0, y=y: color_change(0, y))
+        btnCardArray[0][y].pack(side = LEFT )
             
-        btnCard[1][y] = Button(setCenterFrame, image=click_btn, command = lambda x=1, y=y: color_change(1, y))
-        btnCard[1][y].pack(side = LEFT, fill = BOTH, expand=True)
+        btnCardArray[1][y] = Button(setCenterFrame, image=cardImageArray[1][y], command = lambda x=1, y=y: color_change(1, y))
+        btnCardArray[1][y].pack(side = LEFT)
 
-        btnCard[2][y] = Button(setLowerFrame, image=click_btn, command = lambda x=2, y=y: color_change(2, y))
-        btnCard[2][y].pack(side = LEFT, fill = BOTH, expand=True)
+        btnCardArray[2][y] = Button(setLowerFrame, image=cardImageArray[2][y], command = lambda x=2, y=y: color_change(2, y))
+        btnCardArray[2][y].pack(side = LEFT)
     else:
-        btnCard[0][y] = Button(setUpperFrame, command = lambda x=0, y=y: color_change(0, y))
-        btnCard[0][y].pack(side = LEFT, fill = BOTH, expand=True)
+        btnCardArray[0][y] = Button(setUpperFrame, image=emptyImage, state=DISABLED, bd=0, command = lambda x=0, y=y: color_change(0, y))
+        btnCardArray[0][y].pack(side = LEFT)
             
-        btnCard[1][y] = Button(setCenterFrame, command = lambda x=1, y=y: color_change(1, y))
-        btnCard[1][y].pack(side = LEFT, fill = BOTH, expand=True)
+        btnCardArray[1][y] = Button(setCenterFrame, image=emptyImage, state=DISABLED, bd=0, command = lambda x=1, y=y: color_change(1, y))
+        btnCardArray[1][y].pack(side = LEFT)
 
-        btnCard[2][y] = Button(setLowerFrame, command = lambda x=2, y=y: color_change(2, y))
-        btnCard[2][y].pack(side = LEFT, fill = BOTH, expand=True)
+        btnCardArray[2][y] = Button(setLowerFrame, image=emptyImage, state=DISABLED, bd=0, command = lambda x=2, y=y: color_change(2, y))
+        btnCardArray[2][y].pack(side = LEFT)
 
 
-setMainFrame.pack(fill = BOTH, expand=True)
-setFrame.pack(side = LEFT, fill = BOTH, expand=True)
-setUpperFrame.pack(fill = BOTH, expand=True)
-setCenterFrame.pack(fill = BOTH, expand=True)
-setLowerFrame.pack(fill = BOTH, expand=True)
+setMainFrame.pack(fill = BOTH, expand = True)
+setFrame.pack(side = LEFT, fill = BOTH, expand = True)
+setUpperFrame.pack(expand = True)
+setCenterFrame.pack(expand = True)
+setLowerFrame.pack(expand = True)
 
 window.mainloop()

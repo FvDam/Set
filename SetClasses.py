4c1b1eca06f068a6262d21dcb02200d8a5b130e9
FvDam <92367008+FvDam@users.noreply.github.com>
2022-10-25 23:28:20 +0200

    Added end of game

--- SetClasses.py
+++ SetClasses.py
@@ -5,13 +5,15 @@ from itertools import combinations
 import os
 import random
 import time
+from tkinter import messagebox
 from xml.etree.ElementTree import tostring
 
 # Handy links
 # Password manager:
 # https://github.com/Why-not-dx/PassManager/blob/main/data_base_exercise/app_handle.py
 # https://www.youtube.com/watch?v=CygEKc2bi_0&ab_channel=TILTW
-# 
+# Paper about set
+# https://www.playmonster.com/wp-content/uploads/2019/10/SETPROOF.pdf
 # 
 
 
@@ -74,7 +76,7 @@ class StartPage(tk.Frame):
 		label.grid(row = 0, column = 4, padx = 10, pady = 10)
 
 		button1 = ttk.Button(self, text ="Start Game",
-		command = lambda : controller.show_frame(ActualGame))
+					command = lambda : controller.show_frame(ActualGame))
 	
 		# putting the button in its place by
 		# using grid
@@ -82,7 +84,7 @@ class StartPage(tk.Frame):
 
 		## button to show frame 2 with text layout2
 		button2 = ttk.Button(self, text ="Stats page",
-		command = lambda : controller.show_frame(StatsPage))
+					command = lambda : controller.show_frame(StatsPage))
 	
 		# putting the button in its place by
 		# using grid
@@ -93,9 +95,12 @@ class StartPage(tk.Frame):
 class ActualGame(tk.Frame):
 	def __init__(self, parent, controller):
 		tk.Frame.__init__(self, parent)
+		self.inGameTime = 0
 		self.startTime = time.perf_counter()
 		self.cardsCounter = 0
-		self.counter = 0
+		self.cardsClickedCounter = 0
+		self.score = 0
+		self.wrongCounter = 0
 		self.clickedArray = [[99, 99],[99, 99],[99, 99]]
 		self.cardImageArray = [[0 for y in range(3)] for x in range(5)]
 		self.cardNumberArray = [[['0','0','0','0'] for y in range(3)] for x in range(5)]
@@ -163,13 +168,13 @@ class ActualGame(tk.Frame):
 				self.btnCardArray[x][2].pack(side = LEFT)
 		
 		# Labels
-		lblScore = tk.Label(self.scoreFrame, text = "Score:")
-		lblScore.pack(side = LEFT, expand = True)
+		self.lblScore = tk.Label(self.scoreFrame, text = "Score: 0")
+		self.lblScore.pack(side = LEFT, expand = True)
 
-		lblWrong = tk.Label(self.scoreFrame, text = "Wrong:")
-		lblWrong.pack(side = LEFT, expand = True)
+		self.lblWrong = tk.Label(self.scoreFrame, text = "Wrong: 0")
+		self.lblWrong.pack(side = LEFT, expand = True)
 
-		self.lblTime = tk.Label(self.scoreFrame, text = "Time:")
+		self.lblTime = tk.Label(self.scoreFrame, text = "Time: 0000")
 		self.lblTime.pack(side = LEFT, expand = True)
 		self.updateRunTime()
 
@@ -188,13 +193,14 @@ class ActualGame(tk.Frame):
 		btnCheckCards = tk.Button(self.buttonFrame, text="CheckCards", command = self.checkAllCards)
 		btnCheckCards.pack( side=LEFT,  expand = True)
 
-		btnResetGame = tk.Button(self.buttonFrame, text = "Reset Game",
+		self.btnResetGame = tk.Button(self.buttonFrame, text = "Reset Game",
 							command = lambda : controller.reloadGame())
-		btnResetGame.pack(side=LEFT, expand = True)
+		self.btnResetGame.pack(side=LEFT, expand = True)
 
-		btnStatsPage = tk.Button(self.buttonFrame, text = "Stats page",
+		self.btnStatsPage = tk.Button(self.buttonFrame, text = "Stats page",
 							command = lambda : controller.show_frame(StatsPage))
-		btnStatsPage.pack(side=LEFT, expand = True)
+
+		self.btnStatsPage.pack(side=LEFT, expand = True)
 		self.buttonFrame.pack(fill = BOTH, expand = True)
 	
 	def fillImageButtonArray(self):
@@ -210,7 +216,7 @@ class ActualGame(tk.Frame):
 		if [x, y] in self.clickedArray:
 			for i in range(0,3):
 				if self.clickedArray[i] == [x, y]:
-					self.counter = self.counter - 1
+					self.cardsClickedCounter = self.cardsClickedCounter - 1
 					self.clickedArray[i] = [99, 99]
 					self.btnCardArray[x][y].config(bg="SystemButtonFace")
 		else:
@@ -220,32 +226,53 @@ class ActualGame(tk.Frame):
 					self.clickedArray[i] = [x, y]
 					break
 			self.btnCardArray[x][y].config(background="red")
-			self.counter = self.counter + 1
+			self.cardsClickedCounter = self.cardsClickedCounter + 1
 
 		print(self.clickedArray)
 		
-		if self.counter == 3:
+		if self.cardsClickedCounter == 3:
 			self.refreshButtonImage()
 
-	def checkAllCards(self):
+	def checkAllCards(self):	
 		numberOfSets = 0
 		arrayForCheck = [[99, 99],[99, 99],[99, 99]]
 
+		# If deck contains 15 cards
 		if self.cardNumberArray[4][0][0] != '0':
-			# If deck contains 15 cards
 			for item in combinations(range(15),3):
 				for card in range(0,3):
 					arrayForCheck[card] = [item[card]%5, item[card]//5]
 				numberOfSets = numberOfSets + self.checkCards(arrayForCheck)
-		else:
+		# If deck contains 12
+		elif self.cardNumberArray[3][0][0] != '0':
 			for item in combinations(range(12),3):
 				for card in range(0,3):
 					arrayForCheck[card] = [item[card]%4, item[card]//4]
 				numberOfSets = numberOfSets + self.checkCards(arrayForCheck)
-			if numberOfSets == 0:
+			# If the last 12 cards contain a set don't add non existing cards
+			if numberOfSets == 0 and self.cardsCounter != 81:
 				self.fillCardsToFifteen()
+		# If deck contains 9 cards
+		elif self.cardNumberArray[2][0][0] != '0':
+			for item in combinations(range(9),3):
+				for card in range(0,3):
+					arrayForCheck[card] = [item[card]%3, item[card]//3]
+				numberOfSets = numberOfSets + self.checkCards(arrayForCheck)
+		# If deck contains 6 cards
+		elif self.cardNumberArray[1][0][0] != '0':
+			for item in combinations(range(6),3):
+				for card in range(0,3):
+					arrayForCheck[card] = [item[card]%2, item[card]//2]
+				numberOfSets = numberOfSets + self.checkCards(arrayForCheck)
+		# If deck contains 3 cards will probably never happen
+		elif self.cardNumberArray[0][0][0] != '0':
+			for item in combinations(range(3),3):
+				for card in range(0,3):
+					arrayForCheck[card] = [item[card]%1, item[card]//1]
+				numberOfSets = numberOfSets + self.checkCards(arrayForCheck)
 
 		print(f"Total amount of sets found: {numberOfSets}\n\n",)
+		return numberOfSets > 0
 
 	def checkCards(self, clickedArray):
 		cards = [[],[],[]]
@@ -276,64 +303,117 @@ class ActualGame(tk.Frame):
 			return True
 		else:
 			return False
+	
+	def updatePointCounter(self):
+		typeOfSet = 0
+		cards = [[],[],[]]
+		points = (3,1,2,4)
+		for i in range(0,3):
+			cards[i] = self.cardNumberArray[self.clickedArray[i][0]][self.clickedArray[i][1]]
+		
+		# Check if cards are all te same
+		for atri in range(0, 4):
+			tMatch = 0
+			for card in range(0, 2):
+				if cards[card][atri] == cards[card+1][atri]:
+					tMatch = tMatch + 1
+			if tMatch == 2:
+				typeOfSet = typeOfSet + 1
+
+		self.score = self.score + points[typeOfSet]
+		self.lblScore.config(text = f"score: {self.score: <3} ")
+
+	def reorganiseCards(self, prevAmountRows):
+		remaningCardsY = [0,1,2]
+		for card_x, card_y in self.clickedArray:
+			if card_x == prevAmountRows:
+				remaningCardsY.remove(card_y)
+
+		print("CardsY: ", remaningCardsY)
+
+		cardsYCounter = 0
+		for card_x, card_y in self.clickedArray:
+			if card_x < prevAmountRows:
+				self.cardImageArray[card_x][card_y] = self.cardImageArray[prevAmountRows][remaningCardsY[cardsYCounter]]
+				self.cardNumberArray[card_x][card_y] = self.cardNumberArray[prevAmountRows][remaningCardsY[cardsYCounter]]
+				self.btnCardArray[card_x][card_y].config(bg="SystemButtonFace", image=self.cardImageArray[card_x][card_y])
+				cardsYCounter = cardsYCounter + 1
+		# Disable cards
+		for y in range(3):
+			self.cardNumberArray[prevAmountRows][y] = '0'
+			self.btnCardArray[prevAmountRows][y].config(bg="SystemButtonFace", state=DISABLED, bd=0, image=PhotoImage(file='testEmpty.png'))
 
 	def refreshButtonImage(self):
 		if self.checkCards(self.clickedArray):
-			if self.cardsCounter == 81:
-				print("Game done")
-				
+			self.updatePointCounter()	
 			if self.cardNumberArray[4][0][0] != '0':
-				# If deck contains 15 cards
-				cardsY = [0,1,2]
-				for card_x, card_y in self.clickedArray:
-					if card_x == 4:
-						cardsY.remove(card_y)
-
-				print("CardsY: ", cardsY)
-
-				cardsYCounter = 0
-				for card_x, card_y in self.clickedArray:
-					if card_x < 4:
-						self.cardImageArray[card_x][card_y] = self.cardImageArray[4][cardsY[cardsYCounter]]
-						self.cardNumberArray[card_x][card_y] = self.cardNumberArray[4][cardsY[cardsYCounter]]
-						self.btnCardArray[card_x][card_y].config(bg="SystemButtonFace", image=self.cardImageArray[card_x][card_y])
-						cardsYCounter = cardsYCounter + 1
-				# Disable cards
-				for y in range(3):
-					self.cardNumberArray[4][y] = '0'
-					self.btnCardArray[4][y].config(bg="SystemButtonFace", state=DISABLED, bd=0, image=PhotoImage(file='testEmpty.png'))
-			else:
+				self.reorganiseCards(4)
+			elif self.cardNumberArray[3][0][0] != '0' and self.cardsCounter != 81:
 				for card_x, card_y in self.clickedArray:
 					self.cardImageArray[card_x][card_y] = PhotoImage(file=''.join(map(str, self.cardDeckArray[self.cardsCounter]))+".png")
 					self.cardNumberArray[card_x][card_y] = self.cardDeckArray[self.cardsCounter]
 					self.cardsCounter = self.cardsCounter + 1
-					self.btnCardArray[card_x][card_y].config(bg="SystemButtonFace", image=self.cardImageArray[card_x][card_y])
+					self.btnCardArray[card_x][card_y].config(bg="SystemButtonFace", 
+															image=self.cardImageArray[card_x][card_y])
 				self.lblNumberOfCards.config(text = f"Number of cards:{self.cardsCounter}/81")
 				print(f"Number of cards: {self.cardsCounter}")
+			# Last 12 cards
+			elif self.cardNumberArray[3][0][0] != '0':
+				self.reorganiseCards(3)
+				if self.checkAllCards:
+					tk.messagebox.showinfo(title='Game done!', 
+											message = f"Good job you finished the game in: \n{self.inGameTime} secondes")
+					self.btnResetGame.invoke()
+					self.btnStatsPage.invoke()
+			# Last 9 cards
+			elif self.cardNumberArray[2][0][0] != '0':
+				self.reorganiseCards(2)
+				if self.checkAllCards:
+					tk.messagebox.showinfo(title='Game done!', 
+											message = f"Good job you finished the game in: \n{self.inGameTime} secondes")
+					self.btnResetGame.invoke()
+					self.btnStatsPage.invoke()
+			# Last 6 cards
+			elif self.cardNumberArray[1][0][0] != '0':
+				self.reorganiseCards(1)
+				if self.checkAllCards:
+					tk.messagebox.showinfo(title='Game done!', 
+											message = f"Good job you finished the game in: \n{self.inGameTime} secondes")
+					self.btnResetGame.invoke()
+					self.btnStatsPage.invoke()
+			# Last 3 cards
+			elif self.cardNumberArray[1][0][0] != '0':
+				tk.messagebox.showinfo(title='Game done!', message = f"Good job you finished the game in: \n{self.inGameTime} secondes")
+				self.btnResetGame.invoke()
+				self.btnStatsPage.invoke()
 		else:
 			print("This isn't a set")
+			# Update wrong counter
+			self.wrongCounter = self.wrongCounter + 1
+			self.lblWrong.config(text = f"Wrong: {self.wrongCounter}" )
 			# Set colors back to default if it isn't containing a set
 			for card_x, card_y in self.clickedArray:
 				self.btnCardArray[card_x][card_y].config(bg="SystemButtonFace", image=self.cardImageArray[card_x][card_y])
 
 		# Reset array and counter
 		self.clickedArray = [[99, 99],[99, 99],[99, 99]]
-		self.counter = 0
+		self.cardsClickedCounter = 0
 
 	def fillCardsToFifteen(self):
 		for y in range(3):
-			self.cardImageArray[4][y] = PhotoImage(file=''.join(map(str, self.cardDeckArray[cardsCounter]))+".png")
-			self.cardNumberArray[4][y] = self.cardDeckArray[cardsCounter]
-			cardsCounter = cardsCounter + 1
+			self.cardImageArray[4][y] = PhotoImage(file=''.join(map(str, self.cardDeckArray[self.cardsCounter]))+".png")
+			self.cardNumberArray[4][y] = self.cardDeckArray[self.cardsCounter]
+			self.cardsCounter = self.cardsCounter + 1
 			self.btnCardArray[4][y].config(bg="SystemButtonFace", state=NORMAL, bd=2, image=self.cardImageArray[4][y])
 		
 		# Reset array and counter
 		self.clickedArray = [[99, 99],[99, 99],[99, 99]]
-		self.counter = 0
+		self.cardsClickedCounter = 0
 
 	def updateRunTime(self):
-		self.startTime
-		self.lblTime.config(text = "Time: " + str(int(time.perf_counter()-self.startTime))+ " seconds")
+		# self.lblTime.config(text = f"Time: {int(time.perf_counter()-self.startTime):0>4}")
+		self.lblTime.config(text = f"Time: {self.inGameTime:0>4}")
+		self.inGameTime = self.inGameTime + 1
 		self.lblTime.after(1000, self.updateRunTime)
 
 # third window frame page2
